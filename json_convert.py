19139cae1085ebeadf647181d4b2d3c7108eef6f
ZsomborMeszaros <mzsombor10@gmail.com>
2025-04-09 11:59:31 +0200

    stats.py, test output

--- json_convert.py
+++ json_convert.py
@@ -1,10 +1,10 @@
 import os
 import json
+import random
 import pandas as pd
-import librosa
 import re
 import wave
-from collections import Counter
+from collections import Counter, defaultdict
 
 def time_to_seconds(time_str):
     match = re.match(r"(\d+):(\d+):(\d+),(\d+)", time_str)
@@ -26,8 +26,6 @@ def get_wav_length(wav_path):
 def convert(wav_dir, csv_file, output_json):
 
     data = {}
-    emotion_counter = Counter()
-    sentiment_counter = Counter()
 
     df = pd.read_csv(csv_file)
 
@@ -48,20 +46,68 @@ def convert(wav_dir, csv_file, output_json):
             "sentiment": sentiment
         }
 
-        emotion_counter[emotion] += 1
-        sentiment_counter[sentiment] += 1
+
+    with open("jsons/" + output_json, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4, ensure_ascii=False)
+
+    print(f"Done! Output: {output_json}")
+
+#convert("MELD-wav16k/wav16k_dev", "MELD-labels/dev_sent_emo.csv", "dev.json")
+
+def count(wav_json, output_json):
+    emotion_counter = Counter()
+    sentiment_counter = Counter()
+
+    with open(wav_json, "r", encoding="utf-8") as f:
+        data = json.load(f)
+        for wav_filename, meta in data.items():
+            emotion = meta["emotion"]
+            sentiment = meta["sentiment"]
+            emotion_counter[emotion] += 1
+            sentiment_counter[sentiment] += 1
 
         counter = {
             "emotion": emotion_counter,
             "sentiment": sentiment_counter
         }
 
-    with open("jsons/" + output_json, "w", encoding="utf-8") as f:
-        json.dump(data, f, indent=4, ensure_ascii=False)
-
-    with open("jsons/counter-" + output_json, "w", encoding="utf-8") as f:
+    with open(output_json, "w", encoding="utf-8") as f:
         json.dump(counter, f, indent=4, ensure_ascii=False)
 
-    print(f"Done! Output: {output_json} and counter-{output_json}")
+    print(f"Done! Output: {output_json}")
 
-convert("MELD-wav16k/wav16k_dev", "MELD-labels/dev_sent_emo.csv", "dev.json")
+
+#count("jsons/test.json", "jsons/counter_test.json")
+
+
+def downsample(count_json, wav_json, output_json):
+    with open(count_json, "r", encoding="utf-8") as f:
+        count_data = json.load(f)
+
+    with open(wav_json, "r", encoding="utf-8") as f:
+        wav_data = json.load(f)
+
+    min_count = min(count_data["emotion"].values())
+
+    emotion_groups = defaultdict(list)
+    for wav_filename, metadata in wav_data.items():
+        emotion = metadata["emotion"]
+        emotion_groups[emotion].append((wav_filename, metadata))
+
+    downsampled = {}
+    for emotion, samples in emotion_groups.items():
+        if len(samples) < min_count:
+            print(f"[WARNING] '{emotion}' has only {len(samples)} samples, less than min_count {min_count}")
+            selected = samples
+        else:
+            selected = random.sample(samples, min_count)
+
+        for fname, meta in selected:
+            downsampled[fname] = meta
+    with open(output_json, "w", encoding="utf-8") as f:
+        json.dump(downsampled, f, indent=4, ensure_ascii=False)
+
+    print(f"Done! Output: {output_json}")
+
+
+downsample("jsons/counter_dev.json", "jsons/dev.json", "jsons/ds_dev.json")
